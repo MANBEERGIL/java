d3859a0b8b16ef36b29f21439fd171da5e5784ee
MANBEERGIL <60202053+MANBEERGIL@users.noreply.github.com>
2020-02-02 16:32:16 -0500

    poisttriiing

--- src/com/lambton/variables.java
+++ src/com/lambton/variables.java
@@ -5,10 +5,8 @@ import java.util.Date;
 
 public class variables {
     public static void main(String[] args) {
-        Point point1 =new Point(1,1);
-        Point point2 =point1;
-        point1.x =2;
-        System.out.println(point2);
+        String message ="manbeer kaur";
+        System.out.println(message.toUpperCase());
 
     }
 }
