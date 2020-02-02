3dc2e75a0969634360672596a3f621f030b90476
MANBEERGIL <60202053+MANBEERGIL@users.noreply.github.com>
2020-02-02 16:19:42 -0500

    point variable

--- src/com/lambton/variables.java
+++ src/com/lambton/variables.java
@@ -1,16 +1,14 @@
 package com.lambton;
 
+import java.awt.*;
+import java.util.Date;
+
 public class variables {
     public static void main(String[] args) {
-        byte age =30;
-        age =35;
-        int  numbers = 123_456_789;
-        float marks =65.34F;
-        char letter ='a';
-        System.out.println("value of age variable  =" +age );
-        System.out.println("value of numbers variable="+numbers);
-        System.out.println("value of marks variable="+marks);
-        System.out.println("value of letter variable="+letter);
+        Point point1 =new Point(1,1);
+        Point point2 =point1;
+        point1.x =2;
+        System.out.println(point2);
 
     }
 }
