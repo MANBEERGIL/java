a3de14171b282b17f963b243076d994b1ced50a5
MANBEERGIL <60202053+MANBEERGIL@users.noreply.github.com>
2020-02-02 16:42:35 -0500

    arays

---
+++ src/com/lambton/arrays.java
@@ -0,0 +1,12 @@
+package com.lambton;
+
+import java.util.Arrays;
+
+public class arrays {
+    public static void main(String[] args) {
+        int number[]=new int[5];
+        number[0] =1;
+        number[2]=10;
+        System.out.println(Arrays.toString(number));
+    }
+}
