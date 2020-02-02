9e11cd2516ea9404d92dd9138c4843e5619ac335
MANBEERGIL <60202053+MANBEERGIL@users.noreply.github.com>
2020-02-02 16:02:05 -0500

    Initial commit

---
+++ src/com/lambton/variables.java
@@ -0,0 +1,16 @@
+package com.lambton;
+
+public class variables {
+    public static void main(String[] args) {
+        byte age =30;
+        age =35;
+        int  numbers = 123_456_789;
+        float marks =65.34F;
+        char letter ='a';
+        System.out.println("value of age variable  =" +age );
+        System.out.println("value of numbers variable="+numbers);
+        System.out.println("value of marks variable="+marks);
+        System.out.println("value of letter variable="+letter);
+
+    }
+}
