6ea46f72143305cce37e42c68c304d7f45d99454
MANBEERGIL <60202053+MANBEERGIL@users.noreply.github.com>
2020-02-02 19:39:42 -0500

    arays

---
+++ src/com/lambton/Multidimensional_array.java
@@ -0,0 +1,11 @@
+package com.lambton;
+
+import java.util.Arrays;
+
+public class Multidimensional_array {
+    public static void main(String[] args) {
+        int [][] matrics ={{1,0,0},{0,1,0},{0,0,1}};
+        System.out.println(Arrays.deepToString(matrics));
+
+    }
+}
--- src/com/lambton/arrays.java
+++ src/com/lambton/arrays.java
@@ -7,6 +7,15 @@ public class arrays {
         int number[]=new int[5];
         number[0] =1;
         number[2]=10;
+        System.out.println(number.length);
+
         System.out.println(Arrays.toString(number));
+
+        int array[]={1,6,5,8,7,10};
+        Arrays.sort(array);
+        System.out.println(array.length);
+        System.out.println(Arrays.toString(array));
+
+
     }
 }
